a7dc46836662bcaa5409567e857b3c1687209849
Baruch Rotbart <busyborotbart@gmail.com>
2022-09-06 17:16:18 -0500

    refactored if block

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function solution(string) {
   let arr = string.split("");
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === arr[i].toUpperCase()) {
-      let arr1 = arr.splice(i, 1, ` ${arr[i]}`);
+      arr.splice(i, 1, ` ${arr[i]}`);
     }
   }
   return arr.join("");
